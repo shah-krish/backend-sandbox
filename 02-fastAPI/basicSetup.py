c0399dc1d595d73db9eb86df1e270a9c695a5a18
shah-krish <krishshah1411@gmail.com>
2026-09-15 06:31:29 -0400

    feat: learn fastAPI post

--- 02-fastAPI/basicSetup.py
+++ 02-fastAPI/basicSetup.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
-app = FastAPI() #fastAPI instance created
+app = FastAPI()  # fastAPI instance created
 
-@app.get("/") #fastAPI instance + http method that the user should use + the path
+
+@app.get("/")  # fastAPI instance + http method that the user should use + the path
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+# Extract everything from body (...) and store it in a python dictionary called payload
+async def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title = {payLoad['title']}, content = {payLoad['content']}"}
