34cab0f5e108bb290d0516d8b95615f43bf12669
shah-krish <krishshah1411@gmail.com>
2026-09-15 07:44:39 -0400

    feat: learn get post

--- 02-fastAPI/basicSetup.py
+++ 02-fastAPI/basicSetup.py
@@ -1,9 +1,19 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()  # fastAPI instance created
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True # If the user doesn't provide a value, it returns true instead of an error
+    rating: Optional[int] = None # Defaults to null if no value provided
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": "1"},
+            {"title": "title of post 2", "content": "content of post 2", "id": "2"}]
 @app.get("/")  # fastAPI instance + http method that the user should use + the path
 async def root():
     return {"message": "Hello World"}
@@ -11,11 +21,12 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
 @app.post("/createposts")
-# Extract everything from body (...) and store it in a python dictionary called payload
-async def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title = {payLoad['title']}, content = {payLoad['content']}"}
+# Use the pydantic Post class to make sure we get data exactly how we wanted
+async def create_posts(post: Post):
+    print(post)
+    print(post.dict()) # Can covert easily to dictionary
+    return {"data": post}
